5945233d29bd37e67b9987ea7b7894c77caeefbc
Andy Taing <ataing@uw.edu>
2016-10-10 18:42:04 -0700

    Practice

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -4,10 +4,10 @@
 hometown <- "Redmond"
 
 # Assign your name to the variable `my.name`
-my.name <- "Andy"
+my.name <- "Andy Taing"
 
 # Assign your height to a variable `my.height`
-my.height <- "5.feet.and.11.inches"
+my.height <- "5'11"
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
 puppies <- "2"
@@ -15,14 +15,12 @@ puppies <- "2"
 # Create a variable `puppy.price`, which is how expensive you think a puppy is
 puppy.price <- "$600"
 
-
 # Create a variable `total.cost` that has the total cost of all of your puppies
 total.cost <- "$1200"
 
 # Create a boolean variable `too.expensive`, set to true if the cost is greater than $1,000
-too.expensive <-1200 >1000 #
-
+too.expensive <- 1200 > 1000
+  
 # Create a variable `max.puppies`, which is the nuber of puppies you can afford for 
 max.puppies <- "0"
 
-git add . 
